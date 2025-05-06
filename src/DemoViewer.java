6ad6d908260493e74e797a5ca0ce36e4ebd8e98a
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-05-06 21:21:47 +0100

    rotations

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -58,12 +58,21 @@ public class DemoViewer {
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
 
+                // Handle rotations via sliders
                 double heading = Math.toRadians(headingSlider.getValue());
-                Matrix3 transform = new Matrix3(new double[]{
-                        Math.cos(heading), 0, -Math.sin(heading),
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, Math.sin(heading),
                         0, 1, 0,
-                        Math.sin(heading), 0, Math.cos(heading)
+                        -Math.sin(heading), 0, Math.cos(heading)
                 });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
 
                 for (Triangle t : tris) {
 //                    Path2D path = new Path2D.Double();
